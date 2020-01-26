2feaa442af703e35bc5e7252d0fb2e226ab0ce84
khalid7487 <khalid15-7487@diu.edu.bd>
2020-01-26 22:07:38 +0600

    post requst using postman

--- course-api/src/main/java/io/khalid/springbootstarter/topic/TopicController.java
+++ course-api/src/main/java/io/khalid/springbootstarter/topic/TopicController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,13 +15,18 @@ public class TopicController {
 	@Autowired
 	private TopicServices topiService;
 	
-	@RequestMapping("/topic")
+	@RequestMapping("/topics")
 	public List<Topic> getAllTopic() {
 		return topiService.getAllService();
 	}
 	
-	@RequestMapping("/topic/{id}")
+	@RequestMapping("/topics/{id}")
 	public Topic getTopic(@PathVariable String id) {
 		return topiService.getTopic(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topiService.addTopic(topic);
 	}
 }
--- course-api/src/main/java/io/khalid/springbootstarter/topic/TopicServices.java
+++ course-api/src/main/java/io/khalid/springbootstarter/topic/TopicServices.java
@@ -1,5 +1,6 @@
 package io.khalid.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicServices {
 	
-     List<Topic> topics =	Arrays.asList(
+     List<Topic> topics =new ArrayList<>(	Arrays.asList(
 			new Topic("spring","Spring Framework","spring Framework Description"),
 			new Topic("java","Core java","core java Description"),
 			new Topic("javascript","javascript","javascript Description")
-			);
+			));
      
      public List<Topic> getAllService(){
     	 return topics;
@@ -21,4 +22,10 @@ public class TopicServices {
      public Topic getTopic(String id) {
     	return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
      }
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+		
+	}
+
 }
